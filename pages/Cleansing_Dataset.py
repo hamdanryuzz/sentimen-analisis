7989afb91b76b0a341b901881aeffd45226fd78b
hamdanryuz <hamdanryuz10@gmail.com>
2022-12-05 06:25:44 +0700

    Update Bug 1.1.1

--- pages/Cleansing_Dataset.py
+++ pages/Cleansing_Dataset.py
@@ -18,6 +18,9 @@ nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
+
+  
 
 sl.sidebar.header('SKRTT Team \n`IT Project`  `Sentimen Anlisis`')
 
